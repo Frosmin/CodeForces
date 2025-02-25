c35ecd289e052af7f3f21f1d743339e9f35a8990
Frosmin <simonabastomart@gmail.com>
2025-02-24 20:35:55 -0400

    Refactor función para contar unos en una lista y simplificar la lógica de entrada

--- 2062A.py
+++ 2062A.py
@@ -1,25 +1,41 @@
 
-def contador_unos(t):
-    cont = 0
-    for i in range(0, len(t)):
-        if t[i] == "1":
-            cont += 1
-    return cont
+# def contador_unos(t):
+#     cont = 0
+#     for i in range(0, len(t)):
+#         if t[i] == "1":
+#             cont += 1
+#     return cont
+
+# def ejer(t):
+#     cont = 0            
+#     for j in range (0, len(t)-2):
+#             if t[j] == "1" and t[j+1] == "0" and t[j+2] == "1":
+#                 t[j] = "0"
+#                 t[j+1] = "1"
+#                 t[j+2] = "0"
+#                 cont+=1
+#     print(t.count("1") +cont)
 
 
 
+# lista = []  
+# n = int(input())
+# for i in range(n):
+#         t = list(map(str ,input()))
+#         lista.append(t)
+# for i in range(n):
+#     ejer(lista[i])
+        
+        
+lista = []  
 n = int(input())
 for i in range(n):
-    cont = 0
-    t = list(map(str ,input()))
-    for j in range (0, len(t)-2):
-        if t[j] == "1" and t[j+1] == "0" and t[j+2] == "1":
-            t[j] = "0"
-            t[j+1] = "1"
-            t[j+2] = "0"
-            cont+=1
-        print(t)
-    print(contador_unos(t)+cont)
+        t = list(map(str ,input()))
+        lista.append(t)
+for i in range(n):
+    print(lista[i].count("1"))
+
+
 
 
 
