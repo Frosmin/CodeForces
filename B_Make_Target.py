e8f9359b78b8bdbc41877a79bdd95e3073c44d60
Frosmin <simonabastomart@gmail.com>
2025-03-07 13:09:01 -0400

    Agregar implementación del problema "B - Make Target" en 'B_Make_Target.py' con funciones para llenar e imprimir la matriz

--- B_Make_Target.py
+++ B_Make_Target.py
@@ -1,9 +1,29 @@
 
 
+def llenar(desde, hasta, valor, matriz):
+    for fila in range(desde, hasta):
+        for col in range(desde, hasta):
+            matriz[fila][col] = valor 
+    return matriz
 
+
+def imprimir(matriz):
+    for fila in matriz:
+        print(fila)
+
+
+
+j = lambda n,i : n + 1 - i
 
 
 n = int(input())
-
-for i in range(n):
-    print("#")
+matriz = [[0 for i in range(n)] for j in range(n)]
+print(j(n,1))
+for i in range(n-1):
+    if i % 2 == 0:
+        res = llenar(i+1,(j(n,i)), '*', matriz)
+        imprimir(res)
+    else:
+        res = llenar(i+1, j(n,i), '#', matriz)
+        imprimir(res)
+  
