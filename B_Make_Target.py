ec0ecb90bb81ba02040ec972ba88764a208bf03e
Frosmin <simonabastomart@gmail.com>
2025-03-07 20:56:42 -0400

    Optimizar la función 'llenar' y ajustar la inicialización de la matriz en 'B_Make_Target.py'

--- B_Make_Target.py
+++ B_Make_Target.py
@@ -3,33 +3,25 @@
 def llenar(desde, hasta, valor, matriz):
     n = len(matriz)
     hasta = min(hasta, n)
-    
     for fila in range(desde, hasta):
         for col in range(desde, hasta):
             matriz[fila][col] = valor 
     return matriz
 
-
 def imprimir(matriz):
     for fila in matriz:
         print(''.join(str(elem) for elem in fila))
-
-
-
+        
 j = lambda n,i : n + 1 - i
-
-
 n = int(input())
-matriz = [[0 for i in range(n)] for j in range(n)]
-
+matriz = [['#' for i in range(n)] for j in range(n)]
+if n == 1:
+    print('#')
+    exit()
 for i in range(n):
-    if i == 0:
-        matriz = llenar(i, (j(n,i)), '#', matriz)
-    
-    elif i % 2 == 0:
+    if i % 2 == 0:
         matriz = llenar(i-1,(j(n,i)), '.', matriz)
         
     else:
-        matriz = llenar(i-1, j(n,i), '#', matriz)
-        
+        matriz = llenar(i-1, j(n,i), '#', matriz)     
 imprimir(matriz)
