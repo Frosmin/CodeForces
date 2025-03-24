e4c9371df100597415c94a8e40e2144a8f0be913
Frosmin <simonabastomart@gmail.com>
2025-03-23 21:40:20 -0400

    Modificar la lógica de verificación de letras en "A - Rearranging ABC" para utilizar un contador en lugar de una lista.

--- A_Rearranging_ABC.py
+++ A_Rearranging_ABC.py
@@ -4,14 +4,33 @@ def quitar_letra(l, s):
             s.pop(i)
             return s
 
+
+# ne = ['A', 'B', 'C']
+# letras = []
+# for i in  range(len(s)):
+#     if s[i] in ne:
+#         letras.append(s[i])
+# if len(letras) == 3:
+#     print('Yes')
+# else:
+#     print('No')
 s =list(map(str, input()))
-ne = ['A', 'B', 'C']
-letras = []
-for i in  range(len(s)):
-    if s[i] in ne:
-        letras.append(s[i])
-if len(letras) == 3:
+res = 0
+for char in s:
+    if char == 'A':
+        res += 1
+        break
+for char in s:
+    if char == 'B':
+        res += 1
+        break
+for char in s:
+    if char == 'C':
+        res += 1
+        break
+if res == 3:
     print('Yes')
 else:
     print('No')
+ 
 
