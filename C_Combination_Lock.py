00e75d6d28be51c55c021b5fe8baeaed116d240e
Frosmin <simonabastomart@gmail.com>
2025-03-26 20:52:54 -0400

    Refactor solución del problema "C. Combination Lock" eliminando funciones innecesarias y simplificando la lógica. Agregar nuevo archivo "batle.py" con función de batalla.

--- C_Combination_Lock.py
+++ C_Combination_Lock.py
@@ -1,38 +1,51 @@
-import itertools
+# import itertools
 
-def generar_permutaciones(lista):
-    permutaciones_tuplas = itertools.permutations(lista)
-    permutaciones_listas = [list(perm) for perm in permutaciones_tuplas]
-    permutaciones_listas.pop(0)
-    return permutaciones_listas
+# def generar_permutaciones(lista):
+#     permutaciones_tuplas = itertools.permutations(lista)
+#     permutaciones_listas = [list(perm) for perm in permutaciones_tuplas]
+#     permutaciones_listas.pop(0)
+#     return permutaciones_listas
 
-def coincide(n, permutado, lsita):
-    for i in range(n):
-        if permutado[i] == lsita[i]:
-            return True
-    return False
+# def coincide(n, permutado, lsita):
+#     for i in range(n):
+#         if permutado[i] == lsita[i]:
+#             return True
+#     return False
 
-def buscar_bloqueado(n, permutado , lista):
-    for i in range(n):
-        if coincide(n, permutado, lista):
-            permutado.append(permutado.pop(0))
-        else:
-            return False
-    return True
+# def buscar_bloqueado(n, permutado , lista):
+#     for i in range(n):
+#         if coincide(n, permutado, lista):
+#             permutado.append(permutado.pop(0))
+#         else:
+#             return False
+#     return True
         
 
+
+# cant = int(input())
+# for _ in range(cant):
+#     n = int(input())
+#     lista = list(range(1,n+1)) #numeros
+#     permutaciones = generar_permutaciones(lista)
+
+#     for i in range(len(permutaciones)):
+#         if buscar_bloqueado(n, permutaciones[i], lista):
+#             print(' '.join(map(str, permutaciones[i])))
+#             break
+#     else:
+#         print(-1)
+
+def solve(n):
+    if n % 2 == 0:
+        print(-1)
+        return
+    for i in range(n, 0, -1): 
+        print(i, end=' ')
+    print()
 
 cant = int(input())
 for _ in range(cant):
     n = int(input())
-    lista = list(range(1,n+1)) #numeros
-    permutaciones = generar_permutaciones(lista)
+    solve(n)
+    
 
-    for i in range(len(permutaciones)):
-        if buscar_bloqueado(n, permutaciones[i], lista):
-            print(' '.join(map(str, permutaciones[i])))
-            break
-    else:
-        print(-1)
-
-        
---
+++ batle.py
@@ -0,0 +1,7 @@
+def battle(lista1, lista2):
+    
+
+
+lista1 = [5,3,3]
+lista2 = [4,4,2]
+
