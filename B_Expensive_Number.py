13d7b1d294f36532e1d128dd242c26eb80ccbe25
Frosmin <simonabastomart@gmail.com>
2025-04-08 11:55:07 -0400

    Refactor solución del problema "B. Expensive Number" simplificando la lógica y eliminando el uso de prefijos de ceros.

--- B_Expensive_Number.py
+++ B_Expensive_Number.py
@@ -6,19 +6,24 @@
 #     else:
 #         print(n-1) 
 
+def eliminar_numeros(n):
+    res = 0
+    for i in range(len(n)):
+        if n[i] != '0':
+            res +=1
+    return res-1
+
+def contar_ceros_finales(s):
+    cont = 0
+    for char in s[::-1]:
+        if char == '0':
+            cont += 1
+        else:
+            break        
+    return cont
+    
 
 t = int(input())
 for _ in range(t):
-    s = input().strip()
-    n = len(s)
-    prefix_zeros = [0] * (n + 1)
-    for i in range(1, n + 1):
-        prefix_zeros[i] = prefix_zeros[i-1] + (1 if s[i-1] == '0' else 0)
-    max_length = 1
-    for j in range(n):
-        if s[j] != '0':
-            zeros_before = prefix_zeros[j]
-            current_length = zeros_before + 1
-            if current_length > max_length:
-                max_length = current_length
-    print(n - max_length)
+    n = str(input())
+    print(eliminar_numeros(n) + contar_ceros_finales(n))
