dd69f8f18a009ca2a2d982c67d89143fdb41c095
Frosmin <simonabastomart@gmail.com>
2025-08-26 19:46:06 -0400

    Refactor A_I_Wanna_Be_the_Guy.py para simplificar la lógica de entrada y eliminar código comentado innecesario.

--- A_I_Wanna_Be_the_Guy.py
+++ A_I_Wanna_Be_the_Guy.py
@@ -3,10 +3,19 @@
 
 n  = int(input())
 
-a = list(map(int,input().split()))
-b = list(map(int,input().split()))
-a[0] = a[1]
-b[0] =b[1]
-nueva_lista= set(a+b)
+a = list(map(int,input().split()))[1:]
+b = list(map(int,input().split()))[1:]
 
+# if n > 1:
+#     if len(a) > 1 and len(b)> 1:
+#         a[0] = a[1]
+#         b[0] =b[1]
+# nueva_lista= set(a+b)
+# else:
+#     print("Oh, my keyboard!")
+#     exit()
+
+
+
+nueva_lista= set(a+b)
 print("I become the guy." if len(nueva_lista) == n else "Oh, my keyboard!")
