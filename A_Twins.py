ea4a3f70916f6eb0fc0a70d8d201d1c8b3fd2a65
Frosmin <simonabastomart@gmail.com>
2025-09-01 15:53:31 -0400

    Agregar implementación del problema "A. Twins" de Codeforces y optimizar el código.

--- A_Twins.py
+++ A_Twins.py
@@ -1,35 +1,20 @@
 n = int(input())
 monedas = list(map(int,input().split()[:n]))
+monedas= sorted(monedas,reverse=True)
+
 
 
 suma = sum(monedas)
-necesario = suma//2
-sumatoria = 0
-# res = 0  
-# cantidad_monedas = 0
-# i = 0 
-# while res <= necesario:
-#     sumita = res+monedas[i]
-#     if sumita <= necesario:
-#         res = res+monedas[i]
-#         cantidad_monedas+=1
-#     i+=1
-# print(res)
-# print(cantidad_monedas) 
 
+necesario = suma/2
+sumatoria = 0
 
 if n == 1:
     print(1)
+    exit()
+i = 0
+while sumatoria <= necesario:
+    sumatoria+=monedas[i]
+    i+=1
 
-for i in range(n):
-    if sumatoria < necesario:
-        sumatoria += monedas[i]
-    elif sumatoria == necesario:
-        sumatoria += monedas[i]
-        print(i+1)
-        # print(sumatoria)
-        exit()
-    else:
-        print(i+1)
-        # print(sumatoria)
-        exit()
+print(i)
