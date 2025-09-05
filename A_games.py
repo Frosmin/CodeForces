5fbe72a372200e991809b91b6250da47d6faeef8
Frosmin <simonabastomart@gmail.com>
2025-09-05 12:22:09 -0400

    Restaurar lógica de conteo en el problema "A. Games" y eliminar código comentado.

--- A_games.py
+++ A_games.py
@@ -3,13 +3,10 @@ dic = {}
 for i in range(n):
     a,b = map(int,input().split())
     for item in dic:
-        if item == a:
-           item+=1
-        elif item == b:
-             item+=1
-        else:
-             item = 0
-print(sum(dic.values()))
-           
-           
-   
+        # if item == a:
+        #    item+=1
+        # elif item == b:
+        #      item+=1
+        # else:
+        #      dic[item] = 0
+print(dic)
