ea127214dde5bca94b6188806b608f9f46f2edc5
Frosmin <simonabastomart@gmail.com>
2025-09-06 23:59:36 -0400

    Agregar implementación del problema "A. Dislike of Threes" de Codeforces y eliminar código comentado.

--- A_Dislike_of_Threes.py
+++ A_Dislike_of_Threes.py
@@ -1,16 +1,25 @@
 n = int(input())
-lst = []
-i = 1
+# for _ in range(n):
+#     num = int(input())
+#     if len(lst) < num:
+#         while len(lst) != num:
+#             if i % 3 != 0 and i%10 != 3:
+#                     lst.append(i)
+#                     i+=1
+#             else:
+#                 i+=1
+#     else:
+#         print(lst[num])                  
+#     print(lst[num-1])
+
 for _ in range(n):
     num = int(input())
-    if len(lst) < num:
-        while len(lst) != num:
-            if i % 3 != 0 and i%10 != 3:
-                    lst.append(i)
-                    i+=1
-            else:
-                i+=1
-    else:
-        print(lst[num])                  
-    print(lst[num-1])
+    i = 0
+    res = 0
+    while i < num:
+        res+=1
+        if res % 3 == 0 or res%10 == 3:
+            continue
+        i +=1              
+    print(res)
             
