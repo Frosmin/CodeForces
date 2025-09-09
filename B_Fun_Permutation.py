05aa1aa10f54136c9f99375d56c5289c7b2d2ecb
Frosmin <simonabastomart@gmail.com>
2025-09-09 17:50:48 -0400

    Refactor B_Fun_Permutation.py: eliminar lógica obsoleta y simplificar el cálculo de la permutación.

--- B_Fun_Permutation.py
+++ B_Fun_Permutation.py
@@ -1,28 +1,18 @@
+def euclides(a,b):
+    if b == 0:
+        return a
+    else:
+        return euclides(b, a%b)
+    
+
 t = int(input())
-
-
 for _ in range(t):
+    new = []
     n = int(input())
-    p = list(map(int, input().split()[:n]))
-
-    group0 = [i for i in range(1, n+1) if i % 3 == 0] 
-    group1 = [i for i in range(1, n+1) if i % 3 == 1]  
-    group2 = [i for i in range(1, n+1) if i % 3 == 2]  
-
-    q = [0] * n
-        
+    lst = list(map(int, input().split()[:n]))
     for i in range(n):
-            if p[i] % 3 == 0 and group0:
-                q[i] = group0.pop()
-            elif p[i] % 3 == 1 and group2:
-                q[i] = group2.pop()
-            elif p[i] % 3 == 2 and group1:
-                q[i] = group1.pop()
-            else:
-                if group0:
-                    q[i] = group0.pop()
-                elif group1:
-                    q[i] = group1.pop()
-                else:
-                    q[i] = group2.pop()
-    print(*q)
+        new.append(n-lst[i]+1)
+    print(" ".join(map(str,new)))
+    
+
+    
