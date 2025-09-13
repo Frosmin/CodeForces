38637d839b8c42025b5e49f578740034b82669a5
Frosmin <simonabastomart@gmail.com>
2025-09-13 01:27:25 -0400

    Agregar lógica para la suma de listas de dígitos sin acarreo y optimizar el bucle de búsqueda en C_New_Maths.py

--- C_New_Maths.py
+++ C_New_Maths.py
@@ -31,24 +31,42 @@ def suma_lista_sin_acarreo(lista):
         resultado = suma_sin_acarreo(resultado, numero)
     return resultado
         
-    
+
+
+
+def suma_listas_de_digitos_sin_acarreo(listas):
+    max_len = max(len(l) for l in listas)
+    # Rellenar con ceros a la izquierda
+    listas = [[0]*(max_len - len(l)) + l for l in listas]
+    resultado = []
+    for digitos in zip(*listas):
+        resultado.append(sum(digitos) % 10)
+    return resultado
   
     
-    
+import math  
 
 n = int(input())
 
-for i in range(1,n+1):
+i = math.ceil(n**0.5)
+
+while i < n:
     lista = [int(digito) for digito in str(i)]
     resultados = []
     j = 0
-    resultado = 0
     for item in reversed(lista):
         resultado = multi(lista, item)*10**j
+        resultado = [int(digito) for digito in str(resultado)]
         resultados.append(resultado)
         j+=1
-    if suma_lista_sin_acarreo(resultados) == n:
+    numerito = (suma_listas_de_digitos_sin_acarreo(resultados))
+    numero = int(''.join(map(str, numerito)))
+
+    if numero == n:
         print(i)
         exit()
+
+    i+=1
 print(-1)
+        
         
