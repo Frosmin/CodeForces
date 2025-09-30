3d25576e52806b284833d789298dbec56feec32b
Frosmin <simonabastomart@gmail.com>
2025-09-30 08:50:56 -0400

    Eliminar código comentado innecesario en B_Mexification.py para mejorar la claridad.

--- B_Mexification.py
+++ B_Mexification.py
@@ -1,15 +1,4 @@
-# from collections import deque
 
-# n = int(input())
-# for _ in range(n):
-#     n,k = map(int,input().split())
-#     lst = list(map(int, input().split()[:n]))
-#     lst.sort()
-#     if lst[0] != 0 :
-#        numerito = lst[0]
-#        print(numerito)
-#     else:
-#         print(lst[-1] + 1)
 for _ in range(int(input())):
   n, k = map(int, input().split())
 
