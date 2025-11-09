53cbe8580a5996bdef20a09ef3bafe90e0593d67
Frosmin <simonabastomart@gmail.com>
2025-11-09 16:18:06 -0400

    Refactor condition in A. Quintomania to check for exact values 5 and 7

--- A_Quintomania.py
+++ A_Quintomania.py
@@ -1,10 +1,9 @@
 for _ in range(int(input())):
     n = int(input())
     lst = list(map(int,input().split()[:n]))
-    res = 0
     for i in range (n-1):
         valor=(abs(lst[i]-lst[i+1]))
-        if valor > 7 or valor < 5 :
+        if valor != 7 and valor != 5 :
             print("NO")
             break
     else:
