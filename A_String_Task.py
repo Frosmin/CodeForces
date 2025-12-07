625d719c321714a956ad10572ba6762c3997200e
Frosmin <simonabastomart@gmail.com>
2025-12-07 16:46:47 -0400

    Refactor quitar_vocales function for clarity and consistency in A_String_Task.py

--- A_String_Task.py
+++ A_String_Task.py
@@ -1,41 +1,15 @@
-def quitar_vocales(word):
-    nueva_word = ""
-    word=word.lower()
-    for letra in word:
+def quitar_vocales(palabra):
+    nueva_palabra = ""
+ 
+    for letra in palabra:
         if letra == 'a' or letra == 'e'  or  letra == 'i'or  letra == 'o' or  letra == 'u'or letra == 'y':
             pass
         else:
-            nueva_word +=letra       
-    return nueva_word
+            nueva_palabra +=letra       
+    return nueva_palabra
             
-word = input()
-res = quitar_vocales(word)
-
+palabra = input().strip().casefold()
+res = quitar_vocales(palabra)
+ 
 res = '.'.join(res)
 print('.'+res)
-
-
-
-def quitar_vocales(word):
-    nueva_word = ""
-    for letra in word:
-        if letra == 'a' or letra == 'e'  or  letra == 'i'or  letra == 'o' or  letra == 'u'or letra == 'y':
-            pass
-        else:
-            nueva_word +=letra       
-    return nueva_word
-            
-word = input().strip().casefold()
-res = quitar_vocales(word)
-
-res = '.'.join(res)
-print('.'+res)
-
-
-def quitar_vocales(word):
-    vocales = set('aeiouy')
-    return '.'.join(ch for ch in word if ch not in vocales)
-
-word = input().strip().casefold()
-res = quitar_vocales(word)
-print('.' + res)
